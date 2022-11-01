ce8c790a188e22affaa087d02a2fb8dd03468bcc
Kyle <63376352+smurflo2@users.noreply.github.com>
2022-11-01 12:37:31 -0500

    HW7: getPaymentRecords and recordPayment

--- VolcanoCoin.sol
+++ VolcanoCoin.sol
@@ -19,7 +19,7 @@ contract VolcanoCoin is Ownable {
         uint amount;
         address recipient;
     }
-    mapping(address => Payment[]) public payments;
+    mapping(address => Payment[]) payments;
 
     event supplyIncreaseEvent(uint256 newSupply);
     event transferEvent(uint256 amount, address recipient);
@@ -44,14 +44,21 @@ contract VolcanoCoin is Ownable {
         balances[msg.sender] = balances[msg.sender] - amount;
         balances[recipient] = balances[recipient] + amount;
 
-        payments[msg.sender].push(Payment({amount: amount, recipient: recipient}));
+        recordPayment(msg.sender, recipient, amount);
 
         emit transferEvent(amount, recipient);
     }
     // 16a: We don't need the sender's address here because we already have access to it in msg.sender
     // 16b: If we had the sender's address as a parameter then someone could transfer tokens from an account that isn't theirs
 
-    function getPaymentsArray(address _address) public view returns (Payment[] memory) {
+    // HW7: What's the difference between this and making the 'payments' mapping public?
+    //        This gets you an array of payments and works even if no payements have been made to the specified account
+    //        The public mappying requires passing an index too and will fail if given an invalid index (i.e. if no payments have been made yet)
+    function getPaymentRecords(address _address) public view returns (Payment[] memory) {
         return payments[_address];
+    }
+
+    function recordPayment(address _sender, address _recipient, uint256 _amount) private {
+        payments[_sender].push(Payment({amount: _amount, recipient: _recipient}));
     }
 }
